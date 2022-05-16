dc6ae57cca42da0fdaf62f8602a68696d81a1110
Cüneyt ALSU <cuneytalsu@gmail.com>
2022-05-16 16:25:41 -0700

    DaysOne

---
+++ 100DaysOfSwift/DaysOne.swift
@@ -0,0 +1,64 @@
+//
+//  Days1.swift
+//  100DaysOfSwift
+//
+//  Created by Cüneyt ALSU on 16.05.2022.
+//
+
+import Foundation
+
+
+class DaysOne {
+    
+    func yazdir() {
+        
+        
+    print("-------- DaysOne - PartOne -------")
+    var str = "Hello World"
+    print(str)
+    str = "Goodbye"
+    print(str)
+        
+        
+    print("-------- DaysOne - PartTwo -------")
+    var age = 21
+    print(age)
+    var population = 2_000_0000
+    print(population)
+        
+        
+    print("-------- DaysOne - PartThree -------")
+    var str1 = """
+    This goes
+    over multiple
+    lines
+    """
+    print(str1)
+    var str2 = """
+    This goes \
+    over multiple \
+    lines
+    """
+    print(str2)
+        
+        
+    print("-------- DaysOne - PartFour -------")
+    var pi = 3.14
+    print(pi)
+    var awesome = true
+    print(awesome)
+        
+        
+    var score = 85
+    var scoreSentences = "Your score was \(score)"
+    print(scoreSentences)
+    var results = "The test result are here : \(scoreSentences)"
+    print(results)
+    
+    
+    print("-------- DaysOne - PartSix -------")
+    let taylor = "swift"
+    print(taylor)
+    
+    }
+}
